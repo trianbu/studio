4283acfc598849a786d39ea792d8ebe62220e8e4
Brian Tu <briantu@Brians-MacBook-Pro.local>
2022-03-27 16:42:30 -1000

    website v1

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <div class="header"></div>
+    <div class="container__main">
+      <slot />
+    </div>
+    <div class="container__menu">
+      <ul class="menu">
+        <li><nuxt-link to="/">HOME</nuxt-link></li>
+        <li><nuxt-link to="info">INFO</nuxt-link></li>
+        <li><nuxt-link to="contact">CONTACT</nuxt-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+/* .header {
+    width: 100%;
+    height: 30px;
+    background-color: red;
+    position: sticky;
+} */
+.container__main {
+  /* padding-top: 30px; */
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+}
+.container__menu {
+  max-width: 100px;
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+}
+.menu {
+  list-style-type: none;
+  font-family: Arial, Helvetica, sans-serif;
+}
+li {
+  margin: 10px 0;
+}
+li:hover {
+  color: #BBBBBB;
+}
+a {
+    text-decoration: none;
+    color: inherit;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container__column">
+        <img class="image" src="/images/SH1.jpeg" alt="" />
+        <img class="image" src="/images/SH2.jpeg" alt="" />
+        <img class="image" src="/images/SH3.jpg" alt="" />
+        <img class="image" src="/images/SH4.jpg" alt="" />
+        <img class="image" src="/images/SH5.jpg" alt="" />
+        <img class="image" src="/images/NY1.jpeg" alt="" />
+        <img class="image" src="/images/NY2.jpeg" alt="" />
+        <img class="image" src="/images/NY3.jpeg" alt="" />
+    </div>
+</template>
+
+<style>
+.container__column {
+  max-width: 50%;
+}
+/* @media only screen
+  and (min-width : 1000px) {
+      .container__column {
+          max-width: 1000px;
+      }
+} */
+.image {
+  max-width: 100%;
+  scroll-snap-align: center;
+  padding-top: 50px;
+}
+</style>
